17ad22fa5d85d0abe2ce09ae0d9b2acd6f3bce85
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-07-14 00:28:58 +0600

    completed home

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,21 +5,24 @@ import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
-  description: "something new",
+  description: "Discover & Share AI Prompts",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
         <div className="main">
-          <div className="gradient"></div>
+          <div className="gradient" />
         </div>
+
         <main className="app">
           <Nav />
           {children}
         </main>
-      </body>
-    </html>
-  );
-}
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,8 @@
-const Provider = () => {
-  return (
-    <div>
-      <h1>Provider</h1>
-    </div>
-  );
+"use client";
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
